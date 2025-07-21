9c0cef6f953a144df8ed51bb189d06b40c3ac115
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:57:46 +0700

    rootReducer

--- client/src/reducers/rootReducer.jsx
+++ client/src/reducers/rootReducer.jsx
@@ -1,12 +1,12 @@
-import { combineReducers } from "redux";
+import { combineReducers as reduxCombineReducers } from "redux";
 import authReducer from "./authReducer";
 import calendarReducer from "./calendarReducer";
 import uiReducer from "./uiReducer";
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  ui: uiReducer,
-  calendar: calendarReducer,
+const rootReducer = reduxCombineReducers({
+  auth: (state, action) => authReducer(state, action),
+  ui: (state, action) => uiReducer(state, action),
+  calendar: (state, action) => calendarReducer(state, action),
 });
 
 export default rootReducer;
