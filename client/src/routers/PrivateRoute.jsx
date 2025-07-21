4b2dd34787cb2556de63fac3ab2071b243651ff6
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:59:10 +0700

    PrivateRoute

--- client/src/routers/PrivateRoute.jsx
+++ client/src/routers/PrivateRoute.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ isAuth, children }) => {
-  return isAuth ? children : <Navigate to="/auth/login" />;
+const PrivateRoute = (props) => {
+  const { isAuth, children } = props;
+
+  if (isAuth) {
+    return children;
+  }
+
+  return <Navigate to="/auth/login" />;
 };
 
 PrivateRoute.propTypes = {
   isAuth: PropTypes.bool.isRequired,
-  children: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default PrivateRoute;
