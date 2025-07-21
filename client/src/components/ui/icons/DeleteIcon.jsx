eef5c0c42d28b324f30f62b858c934e2561e9f59
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:52:46 +0700

    DeleteIcon

--- client/src/components/ui/icons/DeleteIcon.jsx
+++ client/src/components/ui/icons/DeleteIcon.jsx
@@ -1,20 +1,22 @@
 function DeleteIcon(props) {
   return (
     <svg
-      stroke="currentColor"
-      fill="none"
-      strokeWidth={0}
-      viewBox="0 0 24 24"
-      height="1.3em"
+      xmlns="http://www.w3.org/2000/svg"
       width="1.3em"
+      height="1.3em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-      />
+      <path d="M3 7h18" />
+      <path d="M8 7v12a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V7" />
+      <path d="M10 11v6" />
+      <path d="M14 11v6" />
+      <path d="M9 4h6a1 1 0 0 1 1 1v2H8V5a1 1 0 0 1 1-1z" />
     </svg>
   );
 }
