a75bdbbbf2848cab5095285a78340be22395bec2
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:58:46 +0700

    AuthRouter

--- client/src/routers/AuthRouter.jsx
+++ client/src/routers/AuthRouter.jsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginScreen from "../components/auth/LoginScreen";
 import RegisterScreen from "../components/auth/RegisterScreen";
-import "../components/auth/auth.css"
+import "../components/auth/auth.css";
 
 const AuthRouter = () => {
+  const routeConfig = [
+    { path: "/auth/register", element: <RegisterScreen /> },
+    { path: "/auth/login", element: <LoginScreen /> },
+    { path: "*", element: <Navigate replace to="/auth/login" /> },
+  ];
+
   return (
     <main className="content">
       <Routes>
-        <Route path="/auth/register" element={<RegisterScreen />} />
-        <Route path="/auth/login" element={<LoginScreen />} />
-
-        <Route path="*" element={<Navigate replace to="/auth/login" />} />
+        {routeConfig.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
       </Routes>
     </main>
   );
 };
+
 export default AuthRouter;
