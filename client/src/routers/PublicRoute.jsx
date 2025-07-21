157d57d3304e9d6cd9ac71801cf2b2cdcda292fd
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:59:28 +0700

    PublicRoute

--- client/src/routers/PublicRoute.jsx
+++ client/src/routers/PublicRoute.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 
-const PublicRoute = ({ isAuth, children }) => {
-  return isAuth ? <Navigate to="/" /> : children;
+const PublicRoute = (props) => {
+  const { isAuth, children } = props;
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
 };
 
 PublicRoute.propTypes = {
   isAuth: PropTypes.bool.isRequired,
-  children: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default PublicRoute;
