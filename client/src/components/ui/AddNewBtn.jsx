851f63487a801e530eb3fc1dbb4fd0b7ae90b740
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:54:07 +0700

    AddNewBtn

--- client/src/components/ui/AddNewBtn.jsx
+++ client/src/components/ui/AddNewBtn.jsx
@@ -6,15 +6,23 @@ import PlusIcon from "./icons/PlusIcon";
 const AddNewBtn = () => {
   const dispatch = useDispatch();
 
-  const handleClickNew = () => {
+  const onNewButtonClick = () => {
     dispatch(eventClearActive());
     dispatch(uiOpenModal());
   };
 
   return (
-    <button className="btn btn-primary btn--floating btn--floating-right" onClick={handleClickNew}>
-      <PlusIcon />
-    </button>
+    <div className="btn-wrapper">
+      <button
+        type="button"
+        className="btn btn-primary btn--floating btn--floating-right"
+        onClick={onNewButtonClick}
+        aria-label="Add new event"
+      >
+        <PlusIcon />
+      </button>
+    </div>
   );
 };
+
 export default AddNewBtn;
