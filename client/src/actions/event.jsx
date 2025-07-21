977df08fe4a2e2dac22a890ab71fcba668247adc
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:49:02 +0700

    event

--- client/src/actions/event.jsx
+++ client/src/actions/event.jsx
@@ -5,20 +5,20 @@ import types from "../types";
 
 export const eventStartLoading = () => {
   return async (dispatch) => {
-    fetchWithToken("events")
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data.ok) {
-          const events = prepareEvents(data.events);
-          dispatch(eventLoaded(events));
-        } else {
-          if (data.msg) Swal.fire("Error", data.msg, "error");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        Swal.fire("Error", "Please, contact the administrator", "error");
-      });
+    try {
+      const resp = await fetchWithToken("events");
+      const data = await resp.json();
+
+      if (data.ok) {
+        const events = prepareEvents(data.events);
+        dispatch(eventLoaded(events));
+      } else if (data.msg) {
+        Swal.fire("Error", data.msg, "error");
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Please, contact the administrator", "error");
+    }
   };
 };
 
@@ -26,60 +26,49 @@ export const eventStartAddNew = (event) => {
   return async (dispatch, getState) => {
     const { id: _id, name } = getState().auth;
 
-    fetchWithToken("events", event, "POST")
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data.ok) {
-          event.id = data.event._id;
-          event.user = {
-            _id,
-            name,
-          };
-          dispatch(eventAddNew(event));
-          Swal.fire(
-            "Saved",
-            `'${event.title}' has been saved successfully.`,
-            "success"
-          );
-        } else {
-          const msgError =
-            data.msg ||
-            data.errors[Object.keys(data.errors)[0]].msg ||
-            "Please, contact the administrator";
-          Swal.fire("Error", msgError, "error");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        Swal.fire("Error", "Please, contact the administrator", "error");
-      });
+    try {
+      const resp = await fetchWithToken("events", event, "POST");
+      const data = await resp.json();
+
+      if (data.ok) {
+        event.id = data.event._id;
+        event.user = { _id, name };
+        dispatch(eventAddNew(event));
+        Swal.fire("Saved", `'${event.title}' has been saved successfully.`, "success");
+      } else {
+        const msgError =
+          data.msg ||
+          data.errors?.[Object.keys(data.errors)[0]]?.msg ||
+          "Please, contact the administrator";
+        Swal.fire("Error", msgError, "error");
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Please, contact the administrator", "error");
+    }
   };
 };
 
 export const eventStartUpdate = (event) => {
   return async (dispatch) => {
-    fetchWithToken(`events/${event.id}`, event, "PUT")
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data.ok) {
-          dispatch(eventUpdate(event));
-          Swal.fire(
-            "Updated",
-            `'${event.title}' has been updated successfully.`,
-            "success"
-          );
-        } else {
-          const msgError =
-            data.msg ||
-            data.errors[Object.keys(data.errors)[0]].msg ||
-            "Please, contact the administrator";
-          Swal.fire("Error", msgError, "error");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        Swal.fire("Error", "Please, contact the administrator", "error");
-      });
+    try {
+      const resp = await fetchWithToken(`events/${event.id}`, event, "PUT");
+      const data = await resp.json();
+
+      if (data.ok) {
+        dispatch(eventUpdate(event));
+        Swal.fire("Updated", `'${event.title}' has been updated successfully.`, "success");
+      } else {
+        const msgError =
+          data.msg ||
+          data.errors?.[Object.keys(data.errors)[0]]?.msg ||
+          "Please, contact the administrator";
+        Swal.fire("Error", msgError, "error");
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Please, contact the administrator", "error");
+    }
   };
 };
 
@@ -87,28 +76,24 @@ export const eventStartDelete = () => {
   return async (dispatch, getState) => {
     const { id } = getState().calendar.activeEvent;
 
-    fetchWithToken(`events/${id}`, {}, "DELETE")
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (data.ok) {
-          dispatch(eventDelete(id));
-          Swal.fire(
-            "Deleted",
-            `The event has been deleted successfully.`,
-            "success"
-          );
-        } else {
-          const msgError =
-            data.msg ||
-            data.errors[Object.keys(data.errors)[0]].msg ||
-            "Please, contact the administrator";
-          Swal.fire("Error", msgError, "error");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        Swal.fire("Error", "Please, contact the administrator", "error");
-      });
+    try {
+      const resp = await fetchWithToken(`events/${id}`, {}, "DELETE");
+      const data = await resp.json();
+
+      if (data.ok) {
+        dispatch(eventDelete(id));
+        Swal.fire("Deleted", "The event has been deleted successfully.", "success");
+      } else {
+        const msgError =
+          data.msg ||
+          data.errors?.[Object.keys(data.errors)[0]]?.msg ||
+          "Please, contact the administrator";
+        Swal.fire("Error", msgError, "error");
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Please, contact the administrator", "error");
+    }
   };
 };
 
