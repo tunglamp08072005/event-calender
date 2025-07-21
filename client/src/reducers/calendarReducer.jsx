b1c71a67d8edcb031c32e8274585204e721d8658
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:57:27 +0700

    calendarReducer

--- client/src/reducers/calendarReducer.jsx
+++ client/src/reducers/calendarReducer.jsx
@@ -1,61 +1,56 @@
 import types from "../types";
 
-const initialState = {
+const initialState = Object.freeze({
   events: [],
   activeEvent: null,
-};
+});
 
 const calendarReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case types.eventLoaded:
-      return {
-        ...state,
-        events: [...action.payload],
-      };
+  if (!action?.type) return state;
 
-    case types.eventSetActive:
-      return {
-        ...state,
-        activeEvent: action.payload,
-      };
+  const handlers = {
+    [types.eventLoaded]: () => ({
+      ...state,
+      events: Array.isArray(action.payload) ? [...action.payload] : [],
+    }),
 
-    case types.eventClearActive:
-      return {
-        ...state,
-        activeEvent: null,
-      };
+    [types.eventSetActive]: () => ({
+      ...state,
+      activeEvent: action.payload,
+    }),
 
-    case types.eventAddNew:
-      return {
-        ...state,
-        events: [...state.events, action.payload],
-      };
+    [types.eventClearActive]: () => ({
+      ...state,
+      activeEvent: null,
+    }),
 
-    case types.eventUpdate:
-      return {
-        ...state,
-        events: state.events.map((event) =>
-          event.id === action.payload.id ? action.payload : event
-        ),
-      };
+    [types.eventAddNew]: () => ({
+      ...state,
+      events: [...state.events, action.payload],
+    }),
 
-    case types.eventDelete:
-      return {
-        ...state,
-        events: state.events.filter(
-          (event) => event.id !== state.activeEvent.id
-        ),
-        activeEvent: null,
-      };
+    [types.eventUpdate]: () => ({
+      ...state,
+      events: state.events.map((event) =>
+        event.id === action.payload.id ? action.payload : event
+      ),
+    }),
 
-    case types.eventClearLogout:
-      return {
-        ...initialState,
-      };
+    [types.eventDelete]: () => ({
+      ...state,
+      events: state.events.filter(
+        (event) => event.id !== state.activeEvent?.id
+      ),
+      activeEvent: null,
+    }),
 
-    default:
-      return state;
-  }
+    [types.eventClearLogout]: () => ({
+      events: [],
+      activeEvent: null,
+    }),
+  };
+
+  return (handlers[action.type] || (() => state))();
 };
 
 export default calendarReducer;
