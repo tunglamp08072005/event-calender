ecf123250c45b89fe46756f056521affbd442145
Pham Tung Lam <tunglamp227@gmail.com>
2025-07-21 22:59:52 +0700

    index

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -1,12 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import rootReducer from "../reducers/rootReducer";
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false, // Bỏ qua cảnh báo liên quan đến moment.js
+});
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false, // Ignore moment.js warnings when dispatching on store
-    }),
+  middleware: customizedMiddleware,
 });
 
 export default store;
